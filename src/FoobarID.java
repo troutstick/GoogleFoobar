7096ac46d41faa0b052ab705ad4fa49d17d2940c
Justin Thein <jthein@berkeley.edu>
2020-03-15 14:39:11 -0700

    factor triangle into its own file

--- src/FoobarID.java
+++ src/FoobarID.java
@@ -1,5 +1,5 @@
 /** @author Justin Thein
- *  Solutions to Problem 1 and the first part of Problem 2 on Google Foobar.
+ *  Solutions to Problem 1 on Google Foobar.
  * */
 
 public class FoobarID {
@@ -29,28 +29,6 @@ public class FoobarID {
         return false;
     }
 
-    /** For problem 2 of Google Foobar.
-     *
-     *  Calculates the output of a function f(x, y), where x and y are positive integers,
-     *  and f(1, 1) outputs 1. The function lists all positive integers in diagonal rows
-     *  pointing to the upper left, and generally, f(x, y) = f(x + 1, y - 1) + 1.
-     *
-     *  I initially tried to solve the problem using recursion, which led to stack overflow
-     *  errors.
-     *  */
-    private static long triangle(long x, long y) {
-        long answer = 0;
-        while (y > 1) {
-            answer += y + x - 2;
-            y--;
-        }
-        answer += (x * (x + 1)) / 2;
-        return answer;
-    }
 
-    /** Some basic sanity checking. */
-    public static void main(String[] args) {
-        System.out.println(triangle(5, 10));
-    }
 }
 
---
+++ src/Triangle.java
@@ -0,0 +1,31 @@
+/** @author Justin Thein
+ *  Solution to Problem 2 on Google Foobar.
+ * */
+public class Triangle {
+
+    /** For problem 2 of Google Foobar.
+     *
+     *  Calculates the output of a function f(x, y), where x and y are positive integers,
+     *  and f(1, 1) outputs 1. The function lists all positive integers in diagonal rows
+     *  pointing to the upper left, and generally, f(x, y) = f(x + 1, y - 1) + 1.
+     *
+     *  I initially tried to solve the problem using recursion, which led to stack overflow
+     *  errors.
+     *
+     *  TODO: Constant time solution.
+     *  */
+    private static long triangle(long x, long y) {
+        long answer = 0;
+        while (y > 1) {
+            answer += y + x - 2;
+            y--;
+        }
+        answer += (x * (x + 1)) / 2;
+        return answer;
+    }
+
+    /** Some basic sanity checking. */
+    public static void main(String[] args) {
+        System.out.println(triangle(5, 10));
+    }
+}
