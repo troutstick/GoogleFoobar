e3b35ffc373e3f3486a94669c99aee701eea048d
Justin Thein <jthein@berkeley.edu>
2020-03-24 20:47:41 -0700

    setup staircase problem

---
+++ src/Staircase.java
@@ -0,0 +1,21 @@
+/** @author Justin Thein
+ *  My solution to one of the problems for Problem 3 of Google Foobar.
+ *  Find the number of "staircase" designs given a certain number of bricks to build them with.
+ *  */
+public class Staircase {
+    /** @param n: the number of bricks allowed to build a staircase with.
+     * Rules:
+     *          Every step must consist of a unique number of bricks.
+     *          Every successive step must be taller than the last.
+     * Example:
+     *          #
+     *          #
+     *          ##
+     *          31
+     * @return the number of unique steps
+     * The only solution to n == 4; one step has 1 brick, the other has 3.
+     * In this case, solution(4) == 1.
+     * */
+    public static int solution(int n) {
+    }
+}
