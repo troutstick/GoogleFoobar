40f27d9e24845603cd2155ceb893734fa4569fb6
Justin Thein <jthein@berkeley.edu>
2020-05-18 12:57:54 -0700

    finish dodgethelasers in py 2

--- dodgethelasers.py
+++ dodgethelasers.py
@@ -1,2 +1,22 @@
+from math import floor
+from math import sqrt
+
 """Python 2.7 implementation of Dodge The Lasers."""
 
+multiplicand = sqrt(2) - 1
+
+def solution(s):
+    """With str input, return a str formatted output."""
+    return str(answer(int(s)))
+
+def answer(n):
+    """Return the sum of integers floor(1*sqrt(2)) to floor(n*sqrt(2))."""
+    if n <= 0:
+        return 0
+    else:
+        n_prime = int(n * multiplicand)
+        return (n * n_prime) + triangle(n) - triangle(n_prime) - answer(n_prime)
+
+def triangle(n):
+    """Return the triangle sum of n."""
+    return (n * (n + 1)) / 2
